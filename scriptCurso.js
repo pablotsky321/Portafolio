a1ec7fe074a15dd0042d75d2b07a0a624b1bd5a4
pablotsky321 <jprestrepo346@gmail.com>
2023-07-31 09:39:42 -0500

    Update

---
+++ scriptCurso.js
@@ -0,0 +1,55 @@
+//Strings
+/*let a="aló";
+let c=new String(" mundo");
+console.log(c);
+console.log(a)
+console.log(a.length);
+*/
+
+//funciones
+/*function imprimir(){
+console.log("uno");
+console.log("dos");
+console.log("tres");
+console.log("cuatro");
+return 5
+}
+console.log(imprimir());
+*/
+
+//Arrays
+/*
+let a=[];
+let b=[1,5,4,["hola","mundo"]]
+
+for(let d=0;d<=10;d++){
+a.push(d)
+}
+
+console.log(a)
+console.log("")
+console.log(b[3])
+*/
+//Objetos
+/*
+let Pablo={
+Nombre:"Pablo",
+Apellido:"Restrepo",
+Edad:20,
+Pasatiempos:["GYM","Estudio","Salir"],
+Contacto:{
+email:"jprestrepo346@gmail.com",
+instagram:"pablo.r4370",
+numero:"3505965600"
+},
+cadenaNum:function(){
+console.log([1,2,3,4,5])
+},
+numero:b=3
+}
+Pablo.Nombre="Oscar";
+Pablo.Contacto.instagram="@friday564"
+console.log(Pablo)
+let a=JSON.stringify(Pablo)
+console.log(a)
+*/
