83436f877adce6ab890980ec97551a5aa03f8870
pablotsky321 <jprestrepo346@gmail.com>
2023-08-01 10:23:06 -0500

    Update

---
+++ CursoJavaScript/ScriptCurso2.js
@@ -0,0 +1,48 @@
+//Objetos literales
+/*const Dog={
+    Nombre,
+    Raza,
+    Edad,
+    ladrar(){
+        console.log("ola")
+    }
+}
+*/
+
+//Parametros REST
+/*
+function suma(A, B, ...C) {
+    let resultado = A + B
+    C.forEach(function (n) {
+        resultado += n
+    })
+    return resultado
+}
+console.log(suma(1,2,3,4,5))
+*/
+//Clases
+/*class Perro{
+nombre="alfonso"
+edad=30
+ladrar(){
+console.log("Guau")
+}
+}
+let a=new Perro();
+a.nombre="Pablo"
+console.log(a)
+*/
+//Funcion constructora
+/*
+function Perro(nombre,edad){
+    this.nombre=nombre
+    this.edad=edad
+    this.sonido=function(){
+    console.log("Aló")
+    }
+}
+let a=new Perro("snoopy",17);
+console.log(a)
+a.sonido()
+a.sonido2
+*/
