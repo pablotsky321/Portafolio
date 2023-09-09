2b713c1678d71252e1445b1c5ffaa19f7182d5fd
pablotsky321 <jprestrepo346@gmail.com>
2023-09-08 19:34:54 -0500

    Updated

---
+++ Cursos/CURSO HTML/Manejo de tablas/scriptTabla.js
@@ -0,0 +1,11 @@
+function alerta(){
+let Nombre =document.getElementsByClassName('Nombre')
+let Apellido= document.getElementsByClassName('Apellido')
+let Genero=document.querySelector('input[name="Genero"]:checked')
+if(Genero){
+    alert(Nombre.value+" "+Apellido.value+" es de genero "+Genero.value);
+}else{
+    alert("Seleccione un genero bobo hpta")
+}
+
+}
